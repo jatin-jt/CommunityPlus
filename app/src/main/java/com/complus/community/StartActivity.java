23808a6d77bd7e8ff52205a745144aed28c085ed
jatin-jt <sachinjatingupta@gmail.com>
2017-09-02 15:48:08 +0530

    Initial commit

---
+++ app/src/main/java/com/complus/community/StartActivity.java
@@ -0,0 +1,16 @@
+package com.complus.community;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        startActivity(new Intent(this,HomeActivity.class));
+    }
+}
