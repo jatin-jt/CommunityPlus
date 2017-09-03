dbbae6585259ba6e4b24daeeb68fd76ebbad9cc8
jatin-jt <sachinjatingupta@gmail.com>
2017-09-03 10:22:30 +0530

    Merge branch 'master' of https://github.com/jatin-jt/CommunityPlus
    # Conflicts:
    #	app/src/main/res/layout/content_rewards.xml

--- app/src/main/java/com/complus/community/ComplaintActivity.java
+++ app/src/main/java/com/complus/community/ComplaintActivity.java
@@ -5,6 +5,13 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.complus.community.models.Complaint;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class ComplaintActivity extends AppCompatActivity {
 
     TextView complaintTitle, complaintDesc, complaintDate, complaintCat, complaintThreat;
@@ -15,6 +22,7 @@ public class ComplaintActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complaint);
 
+        String id = getIntent().getStringExtra("ID");
         complaintTitle = (TextView) findViewById(R.id.complaint_title);
         complaintCat = (TextView) findViewById(R.id.complaint_cat);
         complaintDesc = (TextView) findViewById(R.id.complaint_desc);
@@ -22,7 +30,25 @@ public class ComplaintActivity extends AppCompatActivity {
         complaintThreat = (TextView) findViewById(R.id.complaint_threat);
 
         complaintPic = (ImageView) findViewById(R.id.complaint_pic);
+        FirebaseDatabase.getInstance().getReference().child("complaints").child(id).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Complaint com = dataSnapshot.getValue(Complaint.class);
+                complaintCat.setText(com.getCategory());
+                complaintTitle.setText(com.getTitle());
+                complaintDesc.setText(com.getDesc());
+                complaintDate.setText(com.getDate());
+                complaintThreat.setText(com.getThreatLevel());
+//                if(com.getPicEnabled().equals("true")){
+//                    Glide.with(getApplicationContext()).load()
+//                }
+            }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
 
     }
--- app/src/main/java/com/complus/community/MyComplaints.java
+++ app/src/main/java/com/complus/community/MyComplaints.java
@@ -4,18 +4,31 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 import com.complus.community.models.Complaint;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class MyComplaints extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private static final String TAG = "MyComplaints";
     private ArrayList<Complaint> complaints;
+    private ArrayList<String> ids;
+    DatabaseReference mainRef;
+    ComplaintAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +37,60 @@ public class MyComplaints extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        recyclerView = (RecyclerView)findViewById(R.id.complaints_list);
+        recyclerView = (RecyclerView) findViewById(R.id.complaints_list);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        FirebaseUser fUser = FirebaseAuth.getInstance().getCurrentUser();
+        mainRef = FirebaseDatabase.getInstance().getReference();
+        mainRef.child("users-complaints").child(fUser.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                ids = new ArrayList<String>();
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    ids.add(ds.getKey());
+                    Log.d(TAG, "onDataChange: " + ds);
+                }
+                populateList();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    int count;
+    int i;
+
+    void populateList() {
+        complaints = new ArrayList<>();
+        count = ids.size();
+        i = 0;
+        for(String s:ids) {
+            mainRef.child("complaints").child(s).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    Complaint comp = dataSnapshot.getValue(Complaint.class);
+                    Log.d(TAG, "onDataChange: " + dataSnapshot);
+                    complaints.add(comp);
+                    if (i == count) {
+                        adapter = new ComplaintAdapter(complaints, getApplicationContext());
+                        Log.d(TAG, "populateList: " + adapter.getItemCount());
+                        recyclerView.setAdapter(adapter);
+                        //attach
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+            i++;
+
+        }
+    }
 }
