ea101dc13f0fb9dc5a3e700fdec41c3ac47b67cd
jatin-jt <sachinjatingupta@gmail.com>
2017-09-03 05:52:28 +0530

    Load event details through database

--- app/src/main/java/com/complus/community/EarnActivity.java
+++ app/src/main/java/com/complus/community/EarnActivity.java
@@ -4,6 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.complus.community.models.EarnEvent;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class EarnActivity extends AppCompatActivity {
 
     TextView earnTitle, earnDesc, earnPoints, earnLoc, earnDur;
@@ -13,12 +20,30 @@ public class EarnActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_earn);
 
+        String eId = getIntent().getStringExtra("ID");
+
         earnTitle = (TextView) findViewById(R.id.earn_title);
         earnDur = (TextView) findViewById(R.id.earn_duration);
         earnDesc = (TextView) findViewById(R.id.earn_desc);
         earnLoc = (TextView) findViewById(R.id.earn_location);
         earnPoints = (TextView) findViewById(R.id.earn_points);
 
+        DatabaseReference myRef = FirebaseDatabase.getInstance().getReference().child("earn-events");
+        myRef.child(eId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                EarnEvent ee = dataSnapshot.getValue(EarnEvent.class);
+                earnTitle.setText(ee.getTitle());
+                earnDesc.setText(ee.getDesc());
+                earnDur.setText(ee.getStartdate()+"-"+ee.getEnddate());
+                earnLoc.setText(ee.getLocation());
+                earnPoints.setText(ee.getPoints());
+            }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/complus/community/RewardActivity.java
+++ app/src/main/java/com/complus/community/RewardActivity.java
@@ -5,9 +5,18 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.complus.community.models.EarnEvent;
+import com.complus.community.models.RewardEvent;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class RewardActivity extends AppCompatActivity {
 
-    TextView rewardTitle, rewardPoints;
+    TextView rewardTitle, rewardPoints,rewardDesc;
     ImageView rewardpic;
 
     @Override
@@ -15,9 +24,26 @@ public class RewardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reward);
 
+        String rid = getIntent().getStringExtra("ID");
         rewardTitle = (TextView) findViewById(R.id.reward_title);
         rewardPoints = (TextView) findViewById(R.id.rewards_points);
+        rewardDesc = (TextView) findViewById(R.id.reward_desc);
         rewardpic = (ImageView) findViewById(R.id.reward_pic);
+        DatabaseReference myRef = FirebaseDatabase.getInstance().getReference().child("reward-events").child(rid);
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                RewardEvent re = dataSnapshot.getValue(RewardEvent.class);
+                rewardTitle.setText(re.getTitle());
+                rewardPoints.setText(re.getPoints());
+                rewardDesc.setText(re.getDesc());
+                Glide.with(getApplicationContext()).load(re.getPiclink()).into(rewardpic);
+            }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
